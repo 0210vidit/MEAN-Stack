46ab802fcaffd059e6b3974a8dc1157033fe6872
Vidit Shah <0210vidit@gmail.com>
2020-05-30 14:53:55 +0530

    initial commit

---
+++ posts/src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css'],
+})
+export class PostCreateComponent implements OnInit {
+  newPost = 'No Content';
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onAddPost() {
+    this.newPost = 'This is a new Post';
+  }
+}
